55abf6784533f3fd2f7ceb7b0891043864ca5350
chandan kuiry <kuirychandan@gmail.com>
2016-06-15 04:12:43 +0530

    add forms validation in forms.py

--- src/products/forms.py
+++ src/products/forms.py
@@ -3,5 +3,21 @@ from django import forms
 
 class ProductAddForm(forms.Form):
 	title = forms.CharField()
-	description = forms.CharField() #this might be a problem
+	description = forms.CharField() #this might be a problem if we give it a textfield like model.py
 	price  = forms.DecimalField()
+
+	def clean_price(self):
+		price = self.cleaned_data.get("price")
+		if price <= 1.00:
+			raise forms.ValidationError("Price must be greater than $1.00")
+		elif price >= 99.99:
+			raise forms.ValidationError("Price must be less than $100.00")
+		else:
+			return price
+
+	def clean_title(self):
+		title = self.cleaned_data.get("title")
+		if len(title) > 3:
+			return title
+		else:
+			raise forms.ValidationError("Title must be greater than 3 characters long.")
